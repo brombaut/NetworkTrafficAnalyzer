24757e35d156b75a017a985a9d57fb8631f9abe7
brombaut <ben.rombaut@unb.ca>
2018-11-30 12:16:05 -0400

    Adding comments explaining TCP connection open and close flags

--- code/NetworkTrafficAnalyzer.java
+++ code/NetworkTrafficAnalyzer.java
@@ -46,15 +46,37 @@ public class NetworkTrafficAnalyzer {
   }
 
   public String[] getShellCommand() {
-    // tcpdump -s 0 == Capture all bytes of data within the packet
-    // tcpdump -s 500 == Capture 500 bytes of data for each packet rather than the default of 68 bytes
-    // tcpdump -n == Display IP addresses and port numbers instead of domain and service names
-    //               when capturing packets (note: on some systems you need to specify -nn to display port numbers)
-    // tcpdump -B 524288
-    // tcpdump tcp == Capture only TCP packets
-    // tcpdump udp == Capture only UDP packets
-    // tcpdump -tt == Print the timestamp, as seconds since January 1, 1970, 00:00:00, UTC,
-    //                and fractions of a second since that time, on each dump line.
+    /**
+    tcpdump -s 0 == Capture all bytes of data within the packet
+    tcpdump -s 500 == Capture 500 bytes of data for each packet rather than the default of 68 bytes
+    tcpdump -n == Display IP addresses and port numbers instead of domain and service names
+                  when capturing packets (note: on some systems you need to specify -nn to display port numbers)
+    tcpdump -B 524288
+    tcpdump tcp == Capture only TCP packets
+    tcpdump udp == Capture only UDP packets
+    tcpdump -tt == Print the timestamp, as seconds since January 1, 1970, 00:00:00, UTC,
+                   and fractions of a second since that time, on each dump line.
+
+    Flags
+      client -> server [S]  'Syn'
+      server -> client [S.] 'Syn Ack'
+      client -> server [.]  'Ack'
+      .
+      .
+      .
+      client -> server [P.] 'Push' ? I dont know
+      server -> client [.] 'Ack' ? I dont know
+      .
+      .
+      .
+      server -> client [F.]
+      client -> server [F.]
+      server -> client [.]
+      OR
+      client -> server [F.]
+      server -> client [F.]
+      client -> server [.]
+    */
     String[] shellCommand = new String[] {
             "/bin/bash",
             "-c",
